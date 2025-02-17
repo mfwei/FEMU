7d12d75c4c4b44539ec353a1d623d8d88aa8dcfe
Ming-Feng Wei <mfwei.personal@gmail.com>
2025-02-17 05:36:15 +0000

    Add a script to create git working trees for different versions

---
+++ create-working-trees.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+git worktree add ../femu-v5.2.1  remotes/origin/femu-v5.2.1
+git worktree add ../femu-v7.0.0  remotes/origin/femu-v7.0.0
+git worktree add ../femu-v8.0.0  remotes/origin/femu-v8.0.0
+git worktree add ../femu-v9.0.1  remotes/origin/femu-v9.0.1
