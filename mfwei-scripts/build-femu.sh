7a8ddb5dc4efb90d3ebd0d8c98cbebac32e18a5e
Ming-Feng Wei <mfwei.personal@gmail.com>
2025-02-17 05:56:44 +0000

    Add toolbox for building FEMU

---
+++ mfwei-scripts/build-femu.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+__BUILD_DIR=../build-femu
+
+if [ ! -d $__BUILD_DIR ] ; then
+	mkdir -p $__BUILD_DIR
+	cp -v ../femu-scripts/femu-copy-scripts.sh $__BUILD_DIR
+	cd $__BUILD_DIR
+	./pkgdep.sh
+	cd -
+fi
+
+cd $__BUILD_DIR
+./femu-compile.sh
---
+++ mfwei-scripts/create-working-trees.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+git worktree add ../../femu-v8.0.0  remotes/origin/femu-v8.0.0
+git worktree add ../../femu-v9.0.1  remotes/origin/femu-v9.0.1
